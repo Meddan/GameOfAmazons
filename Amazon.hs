e47b3b33a6060592d7a0f2781d537ca13563a517
Pontus Doverstav <doverstav@hotmail.com>
2014-12-15 11:32:23 +0100

    Added some comments and formatted some old ones

--- Amazon.hs
+++ Amazon.hs
@@ -138,6 +138,7 @@ switchTile :: Tile -> Tile
 switchTile t | t == White = Black
              | t == Black = White
 
+-- Creates the initial board
 initialBoard :: Board
 initialBoard = Board [(topRow Black),blankRow,blankRow,(middleRow Black),blankRow,blankRow,(middleRow White),blankRow,blankRow,(topRow White)]
     where
@@ -159,7 +160,7 @@ gameOverBoard = Board (lastRow:(replicate 9 filledRow))
       lastRow :: [Tile]
       lastRow = [White, Empty] ++ (replicate 6 Arrow) ++ [Black, Empty]
 
---Replaces the given position in the board with the given tile.
+-- Replaces the given position in the board with the given tile.
 replaceM :: Pos -> Tile -> Board -> Board
 replaceM (x,y) t b | not (validPos (x,y)) = b
                    | otherwise = Board (replace y (replace x t row) (rows b))
@@ -176,9 +177,9 @@ replace n a list | n < 0 || (n+1) > length list = list
                  | otherwise = take n list ++ [a] ++ drop (n + 1) list
 
 {-
-Moves the tile at the given position to the other given position.
-Checks if positions are valid and there is a clear path between them.
-Also checks if the piece is an amazon and not empty or arrow.
+  Moves the tile at the given position to the other given position.
+  Checks if positions are valid and there is a clear path between them.
+  Also checks if the piece is an amazon and not empty or arrow.
 -}
 move :: Board -> Pos -> Pos -> Board
 move b p1 p2 | not (clearPath b p1 p2) = b
@@ -189,13 +190,16 @@ move b p1 p2 | not (clearPath b p1 p2) = b
             piece = getPos b p1
 
 -- Gives up, needs better condition
-prop_move :: Board -> Tile -> APos -> Property
-prop_move b t pos = b /= newBoard ==> (getPos newBoard p1 == Empty) && (getPos newBoard p2 == getPos b p1)
-  where p2 = (p pos)
-        p1 = head (findTiles b t)
+prop_move :: Board -> APos -> APos -> Property
+prop_move b pos1 pos2 = b /= newBoard ==> (getPos newBoard p1 == Empty) && (getPos newBoard p2 == getPos b p1)
+  where p2 = (p pos2)
+        p1 = (p pos1)
         newBoard = move b p1 p2
 
---Shoots an arrow to the given space. Checks if the shot is possible but not if there is an amazon available to shoot it.
+{-
+  Shoots an arrow to the given space. Checks if the shot is possible 
+  but not if there is an amazon available to shoot it.
+-}
 shoot :: Board -> Pos -> Pos -> Board
 shoot b p1 p2 | not (clearPath b p1 p2) = b
               | otherwise = replaceM p2 Arrow b
@@ -205,18 +209,23 @@ prop_shoot b pos1 pos2 = b /= newBoard ==> getPos newBoard p2 == Arrow
   where p1 = (p pos1)
         p2 = (p pos2)
         newBoard = shoot b p1 p2
-
---TODO:props
---Not moving at all -> True or false?
+{-
+  Checks if the path between two positions is a straight or diagonal line.
+  If it is, checks if the tiles between them, excluding the first and
+  including the last, are all empty
+-}
 clearPath :: Board -> Pos -> Pos -> Bool
 clearPath b (x1,y1) (x2,y2) | not (validPos (x1,y1) && validPos (x2,y2)) = False
                             | x1 == x2 && y1 == y2 = False
                             | not straightLine = False
                             | otherwise = emptyPath b (x1,y1) (x2,y2)
     where
-        --If we aren't moving straight up or down, we have to be moving the same amount of tiles in both x and y direction.
+        {-
+          If we aren't moving straight up or down, we have to be 
+          moving the same amount of tiles in both x and y direction.
+        -}
         straightLine = ((x1 == x2 ) || (y1 == y2) ) || (abs (x1 - x2) == abs (y1-y2))
-        --Checks if the given path is empty or not
+        -- Checks if the given path is empty or not
         emptyPath :: Board -> Pos -> Pos -> Bool
         emptyPath b (x1,y1) (x2,y2) | y1 == y2 && east = checkPath b (x1,y1) (x2,y2) 1 0
                                     | y1 == y2 = checkPath b (x1,y1) (x2,y2) (-1) 0
@@ -230,18 +239,17 @@ clearPath b (x1,y1) (x2,y2) | not (validPos (x1,y1) && validPos (x2,y2)) = False
             where 
                   east = x2 > x1
                   north = y2 > y1
-        --Checks if the diagonal path given is empty or not.
+        -- Checks if the diagonal path given is empty or not.
         checkPath :: Board -> Pos -> Pos -> Int -> Int -> Bool
         checkPath b (x1,y1) (x2,y2) dx dy | (x1 == x2) && (y1 == y2) = True
                                              | (getPos b ((x1+dx),(y1+dy))) /= Empty = False
                                              | otherwise = checkPath b ((x1+dx),(y1+dy)) (x2,y2) dx dy
 
---Checks if the given position is valid (inside the board)
+-- Checks if the given position is valid (inside the board)
 validPos :: Pos -> Bool
 validPos (x,y) = (x >= 0 && x <=9) && (y >= 0 && y <=9)
 
 --Checks if the game is over and returns the color of the victor, empty if not over.
---TODO: Returning a tile is ugly. Perhaps return (Bool, Maybe tile)?
 gameOver :: Board -> Tile
 gameOver b | overFor b White = Black
            | overFor b Black = White
@@ -272,11 +280,15 @@ tilesAround b (x,y)         | x == 0 && y == 0 = [se,s,e]
                 w = getPos b ((x-1),y)
 
 
---Returns the tile at the given position after checking that it is valid.
+-- Returns the tile at the given position after checking that it is valid.
 getPos :: Board -> Pos -> Tile
 getPos b (x,y) | validPos (x,y) = ((rows b)!!y)!!x
                | otherwise = error "Invalid position"
 
+{-
+  Return a list of the positions of all tiles of the 
+  given type in the given board
+-}
 findTiles :: Board -> Tile -> [Pos]
 findTiles b t = findInRow (rows b) t 0
     where
